341e85e132a74c471affa8ae0953830e6f8106e2
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2025-12-07 17:12:11 +0200

    feat: add application lifespan with async context manager and automatic DB table creation on startup

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Body
 
+from database.db import engine
+from database.models import Base
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all) #there is no logic for parallelizing migrations(from #docs), too rare
+    yield
+    
+#lifespan
+app = FastAPI(lifespan=lifespan)
 
 
-
+#generate short url
 @app.post("/short_url")
 async def generate_short_url(
     long_url: str = Body(embeded=True)
