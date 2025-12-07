98af07111c593f43d66e0c63393291635a1f58d5
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2025-12-07 17:22:08 +0200

    feat(crud): implement add_slug_to_database async function with proper ShortURL persistence

--- crud.py
+++ crud.py
@@ -1,7 +1,16 @@
 from database.db import new_session
+from database.models import ShortURL
 
 async def add_slug_to_database(
     slug:str,
     long_url: str,
     
-)
+):
+    async with new_session() as session:
+        new_slug = ShortURL(
+            slug = slug,
+            long_url = long_url
+        )
+        session.add(new_slug)
+        
+    
