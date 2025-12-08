34772f8cc1eedc4002dc38edbecbe5d6339a89c6
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-08 12:41:02 +0200

    refactor(crud): improve add_slug_to_database with explicit AsyncSession param and better get_long_url_by_slug logic

--- database/crud.py
+++ database/crud.py
@@ -1,34 +1,31 @@
-
-from database.db import new_session
 from database.models import ShortURL
 
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from exceptions import SlugAlreadyExistError
 
 async def add_slug_to_database(
     slug:str,
     long_url: str,
-    
+    session: AsyncSession,
 ):
-    async with new_session() as session:
-        new_slug = ShortURL(
-            slug = slug,
-            long_url = long_url
-        )
-        session.add(new_slug)
-        try:
-            await session.commit()
-        except IntegrityError:
-            raise SlugAlreadyExistError
-        
-async def get_long_url_by_slug_from_db(slug: str)->str | None:
-    async with new_session() as session:
-        query = select(ShortURL).filter_by(slug=slug)
-        result = await session.execute(query)
-        res: ShortURL |  None = result.scalar_one_or_none()
-        return res.long_url if res.long_url else None
+    new_slug = ShortURL(
+        slug = slug,
+        long_url = long_url
+    )
+    session.add(new_slug)
+    try:
+        await session.commit()
+    except IntegrityError:
+        raise SlugAlreadyExistError
+       
+async def get_long_url_by_slug_from_db(slug: str, session)->str | None:
+    query = select(ShortURL).filter_by(slug=slug)
+    result = await session.execute(query)
+    res: ShortURL |  None = result.scalar_one_or_none()
+    return res.long_url if res.long_url else None
         
         
         
