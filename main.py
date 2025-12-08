08cd1362cb39f40213e7da2d1895471b5aed8765
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-08 11:41:23 +0200

    refactor: refactoring main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI(lifespan=lifespan)
 #generate short url
 @app.post("/short_url")
 async def generate_slug(
-    long_url: str = Body(embeded=True)
+    long_url: str = Body(embed=True)
 ):
     new_slug = await generate_short_url(long_url)
     return {"data": new_slug}
